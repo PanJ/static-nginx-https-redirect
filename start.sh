8dd80cef8f97b51bfbee94c39bebeb4375a34c62
Panjamapong Sermsawatsri <panjamapong_s@cleverse.com>
2018-02-19 01:22:16 +0700

    fix bugs

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 
 # From https://github.com/MorbZ/docker-web-redirect
 
-if [ -z "$REDIRECT_TARGET" ]; then
-	echo "Redirect target variable not set (REDIRECT_TARGET)"
-	exit 1
+if [ -z "$PORT" ]; then
+	echo "Defaulting port to 80"
+	PORT=80
 fi
 
 if [ -z "$ROOT_PATH" ]; then
@@ -12,29 +12,19 @@ if [ -z "$ROOT_PATH" ]; then
 	exit 1
 fi
 
-# Add https if not set
-if ! [[ $REDIRECT_TARGET =~ ^https?:// ]]; then
-  REDIRECT_TARGET="https://$REDIRECT_TARGET"
-fi
-
-# Add trailing slash
-if [[ ${REDIRECT_TARGET:length-1:1} != "/" ]]; then
-  REDIRECT_TARGET="$REDIRECT_TARGET/"
-fi
-
-echo "Redirecting HTTP requests to ${REDIRECT_TARGET}..."
-
 cat <<EOF > /etc/nginx/conf.d/default.conf
 server {
-	listen 80;
+  listen $PORT;
   gzip on;
   gzip_disable "msie6";
-  root $ROOT_PATH
-  location / {
-    if ($http_x_forwarded_proto != "https") {
-      rewrite ^(.*)$ $REDIRECT_TARGET\$1 permanent;
-    }
+  location /healthz {
+    access_log off;
+    return 200;
   }
+  if (\$http_x_forwarded_proto != "https") {
+    return 301 https://\$server_name\$request_uri;
+  }
+  root $ROOT_PATH;
 }
 EOF
 
